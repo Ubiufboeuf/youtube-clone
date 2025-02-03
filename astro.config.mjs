ee5cb0b8242f2933ec58a49a8f4228418b6f6f3b
Ubiufboeuf <fede.2007.artencio.u@gmail.com>
2025-02-03 20:42:56 -0300

    cloudflare como adaptador

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,14 +5,11 @@ import react from '@astrojs/react'
 
 import tailwind from '@astrojs/tailwind'
 
-import node from '@astrojs/node';
+import cloudflare from '@astrojs/cloudflare'
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [react(), tailwind()],
   output: 'server',
-
-  adapter: node({
-    mode: 'standalone'
-  })
+  adapter: cloudflare()
 })
