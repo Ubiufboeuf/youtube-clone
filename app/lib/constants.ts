612ca021f03e4574bfe8af3d27c7bc2101d392bc
Ubiufboeuf <fede.2007.artencio.u@gmail.com>
2025-05-02 23:41:44 -0300

    SERVER_URL movido a .env

--- app/lib/constants.ts
+++ app/lib/constants.ts
@@ -1,4 +1,4 @@
-const SERVER_URL = 'http://localhost:1234'
+const SERVER_URL = import.meta.env.SERVER_URL
 export const VIDEO_ENDPOINT = `${SERVER_URL}/video` // de acá se trae el video, el stream de datos
 export const DATA_ENDPOINT = `${SERVER_URL}/data` // endpoint de información, ej: info de un video o creador
 export const BACKGROUND_ENDPOINT = `${SERVER_URL}/background`
