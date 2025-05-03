b532d4b3f54183aa37cd21acabe3936cc1e3f05e
Ubiufboeuf <fede.2007.artencio.u@gmail.com>
2025-05-03 19:35:31 -0300

    Renderizado de home redistribuido

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,12 +1,13 @@
 import { HomeNav } from '@/components/home/HomeNav'
-import VideoCard from '@/components/VideoCard'
-import { useEffect } from 'react'
+import { useEffect, useState, type FC, type JSX, type ReactNode } from 'react'
 import { getAllVideos } from '@/lib/api'
-import { v4 as uuidv4 } from 'uuid'
-// import { useVideosListStore } from '@/stores/useVideosListStore'
-import { Link, useLoaderData } from 'react-router'
+import { useLoaderData } from 'react-router'
 import { AsideMenuMini } from '@/components/home/AsideMenuMini'
 import { user } from '@/lib/mocks'
+import { HomeAnonimous } from '@/components/home/HomeAnonimous'
+import { HomeNoVideos } from '@/components/home/HomeNoVideos'
+import { HomeNormal } from '@/components/home/HomeNormal'
+import type { Video } from '@/env'
 
 export async function clientLoader() {
   try {
@@ -21,9 +22,18 @@ export async function clientLoader() {
 export default function Home () {
   // const [videosSugeridos, setVideosSugeridos] = useState<Video[]>([])
   const videosSugeridos = useLoaderData<typeof clientLoader>()
+  const [HomeMainContent, setHomeMainContent] = useState<FC<{ videosSugeridos: Video[] }>>()
 
   useEffect(() => {
-    // updateVideosSugeridos()
+    let mainContent
+    if (user.type === 'anonimous') {
+      mainContent = () => HomeAnonimous
+    } else if (videosSugeridos.length === 0) {
+      mainContent = () => HomeNoVideos
+    } else {
+      mainContent = () => HomeNoVideos
+    }
+    setHomeMainContent(mainContent)
   }, [])
 
   // async function updateVideosSugeridos () {
@@ -39,40 +49,7 @@ export default function Home () {
       <AsideMenuMini />
       <HomeNav />
       <section id='home' className='absolute top-28 right-0 [transition:width_250ms_ease] flex h-[calc(100%-112px)] w-full ml:w-[var(--nav-width)]'>
-        { user.type === 'anonimous' ? (
-            <div className='fixed left-1/2 top-1/2 [transform:translate(-50%,-50%)] h-fit gap-8 w-124 p-6 bg-neutral-800 border-[1px] border-neutral-600 rounded-xl flex flex-col items-center justify-between'>
-              <h1 className='text-2xl font-bold text-center'>¡Bienvenido a YouTube!</h1>
-              <p className='-mt-2 text-center'>Acá podrás encontrar
-                <strong> videos </strong>y<strong> shorts </strong>
-                de todo tipo, y si no sabes qué buscar, el&nbsp;
-                <Link to='/tv' className='relative w-fit h-fit group inline-flex'>
-                  <span className='opacity-0'>modo TV</span>
-                  <strong className='absolute z-[1] left-1/2 top-1/2 [transform:translate(-50%,-50%)] text-nowrap [background-image:var(--color-pink-gradient)] [background-clip:text] text-transparent'>modo TV</strong>
-                  <strong className='absolute z-[2] left-1/2 top-1/2 [transform:translate(-50%,-50%)] text-nowrap [background-image:var(--color-gradient)] [background-clip:text] text-transparent transition-opacity opacity-0 group-hover:opacity-100'>modo TV</strong>
-                </Link>
-                &nbsp;será lo mejor para tí.
-              </p>
-              <span className='text-neutral-400 text-wrap text-xs text-center'>Necesitas consumir contenido para poder recomendarte cosas, <br />
-                <strong>¡animate a hacer tu primera búsqueda!</strong>
-              </span>
-            </div>
-          ) : videosSugeridos.length === 0 ? (
-            <div className='h-full w-full flex items-center justify-center flex-col gap-4'>
-              <h1 className='text-2xl font-bold'>Hubo un error consiguiendo los videos</h1>
-              <p className='w-112'>
-                Si recargar la página no soluciona el error ponte en contacto con el
-                <a href='#lol' className='text-blue-400'> soporte técnico </a>
-                para solucionar el problema.
-              </p>
-            </div>
-          ) : (
-            <div className='min-h-full h-fit w-full max-w-full grid grid-cols-[min(100%,500px)] ms:grid-cols-[repeat(auto-fill,minmax(312px,1fr))] sm:p-6 ms:gap-4 justify-center items-start'>
-              {
-                videosSugeridos.map(video => <VideoCard key={uuidv4()} video={video} />)
-              }
-            </div>
-          )
-        }
+        { HomeMainContent && <HomeMainContent videosSugeridos={videosSugeridos} /> }
       </section>
     </>
   )
