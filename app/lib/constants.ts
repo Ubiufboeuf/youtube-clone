565911ca0f562427ce8411f4acff2dd4e0b28b7c
Ubiufboeuf <fede.2007.artencio.u@gmail.com>
2025-05-03 15:59:04 -0300

    variable de entorno SERVER_URL a VITE_SERVER_URL

--- app/lib/constants.ts
+++ app/lib/constants.ts
@@ -1,4 +1,4 @@
-const SERVER_URL = import.meta.env.SERVER_URL
+const SERVER_URL = import.meta.env.VITE_SERVER_URL
 export const VIDEO_ENDPOINT = `${SERVER_URL}/video` // de acá se trae el video, el stream de datos
 export const DATA_ENDPOINT = `${SERVER_URL}/data` // endpoint de información, ej: info de un video o creador
 export const BACKGROUND_ENDPOINT = `${SERVER_URL}/background`
