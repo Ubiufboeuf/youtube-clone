0bb131138565391b5315cc53c7f18345b41f8843
Ubiufboeuf <fede.2007.artencio.u@gmail.com>
2025-05-24 14:27:56 -0300

    POSTER_ENDPOINT a THUMBNAIL_ENDPOINT

--- app/lib/constants.ts
+++ app/lib/constants.ts
@@ -2,7 +2,7 @@ const SERVER_URL = import.meta.env.VITE_SERVER_URL
 export const VIDEO_ENDPOINT = `${SERVER_URL}/video` // de acá se trae el video, el stream de datos
 export const DATA_ENDPOINT = `${SERVER_URL}/data` // endpoint de información, ej: info de un video o creador
 export const BACKGROUND_ENDPOINT = `${SERVER_URL}/background`
-export const POSTER_ENDPOINT = `${SERVER_URL}/poster`
+export const THUMBNAIL_ENDPOINT = `${SERVER_URL}/poster`
 export const ERROR_ENDPOINT = `${SERVER_URL}/error`
 
 export const DATA_VIDEO_ENDPOINT = `${DATA_ENDPOINT}/video` // endpoint de información, ej: info de un video o creador
