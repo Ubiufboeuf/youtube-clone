d7dbcb976011f4ecd6c09941dbc2658610941e72
Ubiufboeuf <fede.2007.artencio.u@gmail.com>
2025-05-31 16:43:18 -0300

    chore: :fire: eliminar archivo innecesario
    elimina un archivo innecesario, no se usa y por el momento no se usará algo así

--- app/db/client.ts
+++
@@ -1,7 +0,0 @@
-import { createClient } from '@libsql/client'
-
-const url = import.meta.env.VITE_DB_URL
-const authToken = import.meta.env.VITE_DB_AUTH_TOKEN
-
-export const db = createClient({ url, authToken })
-
