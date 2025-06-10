9dfebef2fa319b90e258ac04593c89b88138ab85
Ubiufboeuf <fede.2007.artencio.u@gmail.com>
2025-06-10 14:51:01 -0300

    fix: allowedHosts para todo host

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,6 @@ import tsconfigPaths from 'vite-tsconfig-paths'
 export default defineConfig({
   plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
   server: {
-    allowedHosts: ['801f-190-64-207-178.ngrok-free.app']
+    allowedHosts: ['*']
   }
 })
