72cb99592a500707ea2f909bb907d66b6a9aca12
Ubiufboeuf <fede.2007.artencio.u@gmail.com>
2025-06-10 20:52:57 -0300

    docs: :bulb: cambiar comentarios para aumentar legibilidad

--- app/lib/constants.ts
+++ app/lib/constants.ts
@@ -1,9 +1,9 @@
 const SERVER_URL = import.meta.env.VITE_SERVER_URL
-export const VIDEO_ENDPOINT = `${SERVER_URL}/video` // de acá se trae el video, el stream de datos
-export const DATA_ENDPOINT = `${SERVER_URL}/data` // endpoint de información, ej: info de un video o creador
+export const VIDEO_ENDPOINT = `${SERVER_URL}/video` // streams de videos
+export const DATA_ENDPOINT = `${SERVER_URL}/data` // información de video/s o creadores
 export const BACKGROUND_ENDPOINT = `${SERVER_URL}/background`
 export const THUMBNAIL_ENDPOINT = `${SERVER_URL}/poster`
 export const ERROR_ENDPOINT = `${SERVER_URL}/error`
 export const AVATAR_ENDPOINT = `${SERVER_URL}/avatar`
 
-export const DATA_VIDEO_ENDPOINT = `${DATA_ENDPOINT}/video` // endpoint de información, ej: info de un video o creador
+export const DATA_VIDEO_ENDPOINT = `${DATA_ENDPOINT}/video` // información de un video
