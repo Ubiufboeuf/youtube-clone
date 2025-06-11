31b34c5f7c73ec7e5021d2a9e6c680995b3d024f
Ubiufboeuf <fede.2007.artencio.u@gmail.com>
2025-06-10 23:00:05 -0300

    build: arreglar allowedHosts

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,6 @@ import tsconfigPaths from 'vite-tsconfig-paths'
 export default defineConfig({
   plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
   server: {
-    allowedHosts: ['https://youtube-clone-1d8.pages.dev']
+    allowedHosts: ['youtube-clone-1d8-pages-dev', 'bc5d-2800-a4-1030-fe00-fffc-9d2f-f701-2853.ngrok-free.app']
   }
 })
